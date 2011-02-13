ba110691ae008340f9f36d4d18c22657ce3e638d
Emil Sedgh <emilsedgh@gmail.com>
2011-02-13 12:38:55 +0330

    adding entry for next week's entry. i reserverd 126'th session for last weeks release party. waiting for irix to give us photos

---
+++ contents/entries/127.php
@@ -0,0 +1,21 @@
+<div class="type">
+	Session
+</div>
+
+<div class="title">
+	جلسه‌ی یکصد و بیست و ششم
+</div>
+
+<div class="subject">
+موضوع جلسه هنوز مشخص نشده است.
+</div>
+
+<div class="date">
+1389/11/26
+</div>
+
+<div class="body">
+جلسه از ساعت ۱۷ الی ۱۹ برگزاد خواهد شد.
+<br />
+اگر علاقه به ارائه ملطبی دارید، به ما اطلاع دهید. 
+</div>
