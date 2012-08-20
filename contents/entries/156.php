b84302c63d7ee7344cee8af7a591e912756fdb9e
root <root@mint>
2012-08-20 19:53:42 +0430

    darje khabare 156

---
+++ contents/entries/156.php
@@ -0,0 +1,28 @@
+<div class="type">
+	Session
+</div>
+
+<div class="title">
+جلسه‌ی یکصد و پنجاه و پنجم
+</div>
+
+<div class="subject">
+Twitter Bootstrap
+</div>
+
+<div class="date">
+1391/05/18
+</div>
+
+<div class="body">
+این جلسه در روز چهار‌شنبه مورخ ۱۸ مردادماه ۱۳۹۱ از ساعت ۱۸ الی ۱۹:۳۰ در فضای آزاد پارک لاله برگزار ‌شد.<br />در این جلسه آقای افشین مهربانی به توضیح در مورد Source Controlها مانند git و svn پرداختند.
+
+
+
+</div>
+
+
+
+
+<br />
+<br />
