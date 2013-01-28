b3a02a49768e436f59c14aa103d2387642840ed4
Ashkan Ghassemi <ghassemi@ftml.net>
2013-01-28 11:30:39 +0330

    166 added to tree.

---
+++ contents/entries/166.php
@@ -0,0 +1,19 @@
+<div class="type">
+	Session
+</div>
+
+<div class="title">
+جلسه‌ی یکصد و شصت و ششم
+</div>
+
+<div class="subject">
+توزیع فدورا
+</div>
+
+<div class="date">
+1391/11/11
+</div>
+
+<div class="body">
+در این جلسه آقای مولا پهنادایان  در ارتباط با توزیع فدورا ارائه مطلب خواهند داشت.
+</div>
