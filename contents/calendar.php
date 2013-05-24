33b8d95e393948f8abe00e987692e7ce19fb9baa
smmsadrnezh <smmsadrnezh@gmail.com>
2013-05-24 15:47:41 +0430

    Add Help and Description for Google Calendar.

--- contents/calendar.php
+++ contents/calendar.php
@@ -1,3 +1,8 @@
+<p dir="rtl" style="text-align: right;">&nbsp;</p>
+<p dir="rtl" style="text-align: right;">استفاده از خدمات تقویم گوگل برای اطلاع‌رسانی جلسات توسط تهران‌لاگ به معنی تایید کارکرد این شرکت نبوده و گروه کاربران گنو/لینوکس تهران مسئولیتی در رابطه به سیاست حفظ اسرار (Privacy Policy) این شرکت ندارد.</p>
+<p dir="rtl" style="text-align: right;">&nbsp;</p>
+<p style="text-align: right;" dir="rtl"><span style="color: #ff0000;"><strong>راهنمای اضافه کردن <a href="http://www.google.com/calendar/embed?src=bmExc3JmZWIxczZjazcxdnZ0N2RmMGx2Ym9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ" target="_blank"><span style="color: #ff0000;">تقویم جلسات تهران لاگ</span></a><span style="color: #ff0000;"> به Google Calendar</span></strong></span><br />۱. راه اول:<br /><a href="http://www.google.com/calendar/embed?src=bmExc3JmZWIxczZjazcxdnZ0N2RmMGx2Ym9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ" target="_blank">صفحه‌ی تقویم جلسات تهران لاگ</a> را باز کنید. گوشه‌ی سمت راست پایین صفحه روی علامت&nbsp;<img src="http://www.fsug.ir/wiki/images/5/54/Calendar_plus_en.gif" /> کلیک کنید.<br /> ۲. راه دوم:<br />به <a href="https://www.google.com/calendar" target="_blank">تقویمتان در گوگل</a> <span></span><span></span>بروید. روی فلش رو به پایین کنار Other calendars کلیک کنید. از منویی که ظاهر می‌شود Add by URL را انتخاب کنید. آدرس تقویم را با فرمت بالا یا با فرمت iCal شرکت اپل وارد کنید و روی Add Calendar کلیک کنید.</p>
+<div dir="rtl">&nbsp;</div>
 <div style="text-align: center;">
     <iframe src="https://www.google.com/calendar/embed?src=na1srfeb1s6ck71vvt7df0lvbo%40group.calendar.google.com&ctz=Asia/Tehran" style="border: 0; margin:auto;" width="800" height="600" frameborder="0" scrolling="no"></iframe>
 </div>
