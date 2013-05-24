5c60e862adb35e0b002f12061c0ad0cd017f5527
Ashkan Ghassemi <ghassemi@ftml.net>
2013-05-24 14:01:08 +0430

    google calendar language changed to Farsi. We might want to switch to a more sane solution for handling calendars: possibly publishing iCal or something similar.

--- contents/calendar.php
+++ contents/calendar.php
@@ -1,3 +1,4 @@
 <div style="text-align: center;">
-    <iframe src="https://www.google.com/calendar/embed?src=na1srfeb1s6ck71vvt7df0lvbo%40group.calendar.google.com&ctz=Asia/Tehran" style="border: 0; margin:auto;" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+    <iframe src="https://www.google.com/calendar/embed?src=na1srfeb1s6ck71vvt7df0lvbo%40group.calendar.google.com&ctz=Asia/Tehran&hl=fa" style="border: 0; margin:auto;" width="1024" height="600" frameborder="5" scrolling="no"></iframe>
 </div>
+
