dfd2f9586d92d8f7cf0c6d9dedcdb41af40fd49e
Ashkan Ghassemi <ghassemi@ftml.net>
2013-06-19 02:51:13 +0430

    174 update #2. 175 added.

---
+++ contents/entries/175.php
@@ -0,0 +1,23 @@
+<div class="type">
+	Session
+</div>
+
+<div class="title">
+جلسه‌ی یکصد و هفتاد و پنجم
+</div>
+
+<div class="subject">
+شل‌کد
+</div>
+
+<div class="date">
+1392/04/04
+</div>
+
+<div class="body">
+در این جلسه آقای عرفان امیدفر به معرفی شل‌کد و نحوه‌ی نوشتن آن خواهند پرداخت.
+<br />
+<br />
+</div>
+
+
