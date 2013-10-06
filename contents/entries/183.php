a434b2476ea42e4e934e99258fecdc1243d4f3c5
Behnam Tavakkoli kermani <b.tavakkoli@gmail.com>
2013-10-06 22:54:37 +0330

    jalase 183

---
+++ contents/entries/183.php
@@ -0,0 +1,24 @@
+<div class="type">
+	Session
+</div>
+
+<div class="title">
+جلسه‌ی یکصد و هشتاد و سوم
+</div>
+
+<div class="subject">
+الگوهای مقیاس‌پذیری، در دسترس بودن و پایداری
+</div>
+
+<div class="date">
+1392/07/23
+</div>
+
+<div class="body">
+در این جلسه آقای <a href="http://blog.efazati.org/">محمد افاضاتی</a> به بحث در مورد  ​​Scalability, Availability & Stability Patterns خواهند پرداخت.<br>
+در صورتی که تمایل دارید در جلسه ۱۸۴ (۷ آبان‌ماه) به بحث در مورد موضوع فنی‌ای بپردازید، از همین الآن در <a href="http://tehlug.org/index.php?page=list">میلینگ لیست تهران‌لاگ</a> مطرح نمایید.
+<br><br>
+<b>توجه داشته باشید که جلسه ۱۸۳ تهران‌لاگ از ساعت ۱۸ الی ۱۹ برگزار خواهد گردید.</b>
+
+<br />
+</div>
