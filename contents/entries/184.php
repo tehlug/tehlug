2f504bbb114cd699342d14d882e87ebef73ee076
Behnam Tavakkoli kermani <b.tavakkoli@gmail.com>
2013-10-30 18:30:23 +0330

    ezafe kardan asami

--- contents/entries/184.php
+++ contents/entries/184.php
@@ -15,7 +15,6 @@
 </div>
 
 <div class="body">
-<<<<<<< HEAD
 در این جلسه آقای <a href="http://saeedgnu.blog.ir">سعید رسولی</a>، نویسنده <a href="https://github.com/ilius/starcal">برنامه StarCalendar</a> به بحث در مورد  Internationalization & Localization پرداختند. سپس از حاضرین جلسه در مورد برنامه‌ها و اجرای جلسات لاگ نظرخواهی به عمل آمد.
 <br><br>
 <a href="/presentations/92_08_07_internationlization_localization.pdf" class="attachment">دانلود نسخه‌ی پی-دی-اف ارائه الگوهای مقیاس‌پذیری، در دسترس بودن و پایداری</a>
