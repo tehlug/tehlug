4ad91cb053ea40b260b6733587431393fb8371df
Behnam Tavakkoli kermani <b.tavakkoli@gmail.com>
2013-12-08 19:45:11 +0330

    ezafe kardan asami 186 va khabare 187

---
+++ contents/entries/187.php
@@ -0,0 +1,26 @@
+<div class="type">
+	Session
+</div>
+
+<div class="title">
+جلسه‌ی یکصد و هشتاد و هفتم
+</div>
+
+<div class="subject">
+متدهای توسعه نرم‌افزار BDD و TDD
+</div>
+
+<div class="date">
+1392/09/19
+</div>
+
+<div class="body">
+در این جلسه آقای <a href="http://cyberrabbits.net">فرود غفوریان</a> به بحث در مورد <a href="http://en.wikipedia.org/wiki/Test-driven_development">TDD</a> و <a href="http://en.wikipedia.org/wiki/Behavior-driven_development">BDD</a> خواهند پرداخت.<br><br>
+
+در صورتی که تمایل دارید در جلسه ۱۸۸ (۳ دی‌ماه) به بحث در مورد موضوع فنی‌ای بپردازید یا مدیریت یک بحث اینتراکتیو در مورد موضوعی مشخص را بر عهده بگیرید، از همین الآن در <a href="http://tehlug.org/index.php?page=list">میلینگ لیست تهران‌لاگ</a> مطرح نمایید.<br><br>
+<b>
+توجه داشته باشید که جلسه‌ی ۱۸۷ تهران‌لاگ از ساعت ۱۸ الی ۲۰ در محل سالن آموزش شرکت توسن به نشانی خیابان آفریقا – کوچه نور (آفریقا به سمت شمال، بعد از ظفر – قبل از پمپ بنزین) پلاک ۱۸ برگزار خواهد گردید.
+</b>
+
+<br />
+</div>
