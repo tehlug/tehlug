0131581b5e0339ab5b87304f030450c53dac3545
Behnam Tavakkoli kermani <b.tavakkoli@gmail.com>
2014-04-09 18:08:12 +0430

    ezafe kardan asami va file 194 va khabare namaloom 195

---
+++ contents/entries/195.php
@@ -0,0 +1,21 @@
+<div class="type">
+	Session
+</div>
+
+<div class="title">
+جلسه‌ی یکصد و نود و پنجم
+</div>
+
+<div class="subject">
+در حال حاضر نامشخص
+</div>
+
+<div class="date">
+1393/02/02
+</div>
+
+<div class="body">
+در صورتی که تمایل دارید موضوعی فنی در این جلسه ارائه دهید یا مدیریت بحث آزاد کاربران در مورد موضوعی مشخص را بر عهده بگیرید، همین حالا در <a href="http://tehlug.org/index.php?page=list">میلینگ لیست </a>General تهران‌لاگ مطرح نمایید.
+
+<br />
+</div>
