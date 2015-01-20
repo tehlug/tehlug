632b2e50bc96e4014106f81997f0352d12c642fc
Ashkan Ghassemi <ghassemi@ftml.net>
2015-01-20 19:17:47 +0330

    209 added.

---
+++ contents/entries/209.php
@@ -0,0 +1,21 @@
+<div class="type">
+	Session
+</div>
+
+<div class="title">
+جلسه‌ی دویست و نهم
+</div>
+
+<div class="subject">
+انقلاب جاوا اسکریپت
+</div>
+
+<div class="date">
+1393/11/08
+</div>
+
+<div class="body">
+‌در این جلسه آقای توحید ارسطو در مورد جاوا اسکریپت و تاریخچه‌ی آن صحبت خواهند کرد.
+<br />
+<br />
+</div>
