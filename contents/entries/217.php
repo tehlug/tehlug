01efc1d6aff6d08888177cbd0fd90a089934bc67
Mohammad Reza Kamalifard <mr.kamalifard@gmail.com>
2015-07-27 01:53:44 +0430

    add 217 to sessions

---
+++ contents/entries/217.php
@@ -0,0 +1,54 @@
+<div class="type">
+	Session
+</div>
+
+<div class="title">
+جلسه دویست و هفدهم
+</div>
+
+<div class="subject">
+Reactive Manifesto
+</div>
+
+<div class="date">
+1394/04/03
+</div>
+
+<div class="body">
+در این جلسه آقای رضا سامعی در مورد Reactive Manifesto ارائه ای داشتند. 
+<br />
+این جلسه با توجه به ماه رمضان یک ساعت زودتر از ساعت ۱۷ الی ۱۹ در محل نظام صنفی رایانه ای استان تهران برگزار شد.
+<br />
+<br />
+<a href="/presentations/94-04-03_theReactiveManifesto.pdf" class="attachment">فایل ارائه آقای سامعی در مورد Reactive Manifesto </a><br />
+<br />
+</div>
+
+<ul class="members bullet">
+<li>بهنام توکلی کرمانی</li>
+<li>زهرا توکلی کرمانی</li>
+<li>محمد افاضاتی</li>
+<li>محمدرضا کمالی فرد</li>
+<li>رضا شالباف زاده</li>
+<li>رامین فیاض زاده</li>
+<li>علی جعفرآبادی</li>
+<li>دیبا یوسفی زاد</li>
+<li>علی رستمی</li>
+<li>کاظم حمیدی</li>
+<li>صدرا شیرازی</li>
+<li>مصطفی میرموسوی</li>
+<li>محمد عبدلی</li>
+<li>مجید مصطفوی</li>
+<li>پریسا شیخی</li>
+<li>کاوه محمدی</li>
+<li>علی رهبر </li>
+<li>مرتضی پروینی</li>
+<li>مهدی حمیدی</li>
+<li>امیر حبیب زاده</li>
+<li>محمدحسین چهاردولی</li>
+<li>بیاضی</li>
+<li>حمیدرضا سلیمانی</li>
+<li>رضا سامعی</li></ul>
+<br />
+<br />
+<br />
