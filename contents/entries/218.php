4c5ea015815fd11a1fe70f644bf668ecb8aaef41
Mohammad Reza Kamalifard <mr.kamalifard@gmail.com>
2015-07-27 01:54:02 +0430

    add new session 218

---
+++ contents/entries/218.php
@@ -0,0 +1,21 @@
+<div class="type">
+	Session
+</div>
+
+<div class="title">
+جلسه دویست و هجدهم
+</div>
+
+<div class="subject">
+بحث آزاد
+</div>
+
+<div class="date">
+1394/04/24
+</div>
+
+<div class="body">
+جلسه به صورت بحث آزاد برگزار شد
+<br />
+<br />
+</div>
