09b8b0e0814efb4a14d08d8bf93595538c42e642
Mohammad Reza Kamalifard <mr.kamalifard@gmail.com>
2015-08-03 02:24:33 +0430

    fix link

--- contents/front.php
+++ contents/front.php
@@ -72,6 +72,6 @@
 	</div>
 
 <div style="width: 90%;">
-	<a href="images/tehlug200big.jpg"><img src="images/tehlug200-small.jpg”></a>
+	<a href="images/tehlug200big.jpg"><img src="images/tehlug200-small.jpg"></a>
 	</div>
 </div>
