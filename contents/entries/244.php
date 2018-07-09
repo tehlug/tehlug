e81b817895fce0e1f5c77ae0db8d24b8d5fdde4b
Fayyazzadeh <Faza@openmailbox.org>
2018-07-09 14:24:37 +0430

    TehLUG & TehPUG Presentation

--- contents/entries/244.php
+++ contents/entries/244.php
@@ -18,10 +18,10 @@
 جلسهٔ ۲۴۴ گروه کاربران گنو/لینوکس تهران در روز پنجشنبه، ۲۸ تیر ماه از ساعت ۱۴:۳۰ الی ۱۹ در محل شتابدهنده فینووا واقع در  « دانشگاه امیرکبیر، سالن فجر » برگزار خواهد شد. شرکت در این رویداد همانند جلسات گذشته برای کلیه علاقه‌مندان، آزاد و رایگان می‌باشد.
 <br />
 <br />
-<a href="https://evand.com/TehLUG" class="notice">ثبت‌نام رایگان جهت حضور در جلسه</a>
-<a href="https://goo.gl/forms/y4qHHkNJ9DY4LElF2" class="notice">فرم ارسال ارائه برای جلسات آینده</a>
+<a href="https://evand.com/TehLUG" target="_blank" class="notice">ثبت‌نام رایگان جهت حضور در جلسه</a>
+<a href="https://goo.gl/forms/y4qHHkNJ9DY4LElF2" target="_blank" class="notice">فرم ارسال ارائه برای جلسات آینده</a>
 <br />
-<a href="https://doc.scrapy.org/en/latest/intro/tutorial.html" class="attachment">برای آشنایی و بازدهی بیشتر در کارگاه وب‌کاوی از این لینک استفاده کنید</a>
+<a href="https://doc.scrapy.org/en/latest/intro/tutorial.html" target="_blank" class="attachment">برای آشنایی و بازدهی بیشتر در کارگاه وب‌کاوی از این لینک استفاده کنید</a>
 </div>
 <div>
 <br />
@@ -41,7 +41,7 @@
 از ساعت ۱۶:۳۰ الی ۱۹ جلسه گروه کاربران لینوکس تهران برگزار خواهد شد.
 <br />
 <br />
-ارائهٔ اول توسّط آقای کیان جلالی با موضوع Introduction to Akka(actor model) ,and Reactive systems خواهد بود.
+ارائهٔ اول توسّط آقای کیان جلالی با موضوع Introduction to Akka (actor model) ,and Reactive systems خواهد بود.
 <br />
 
 <br />
