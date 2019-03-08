6082a3c4d2cfb959d6767aff0594ddf90622d397
fayyazzadeh <raminfayyaz@live.com>
2019-03-08 21:35:24 +0330

    masood abkenar present 248

--- contents/entries/248.php
+++ contents/entries/248.php
@@ -21,6 +21,7 @@
 
 <a href="https://goo.gl/forms/y4qHHkNJ9DY4LElF2" target="_blank" class="notice">فرم ارسال ارائه برای جلسات آینده</a>
 <a href="https://evand.com/TehLUG" target="_blank" class="notice">ثبت‌نام رایگان جهت حضور در جلسه</a>
+<a href="https://archive.org/details/TEHLUG249NextcloudMasoudAbkenar720p" class="attachment">فیلم ارائه مسعود آبکنار با موضوع آشنایی با Next Cloud‎</a>
 </div>
 <div>
 <br />
