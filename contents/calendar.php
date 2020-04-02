aba9aeaf608c792a6584db99d8961e8a2e71245a
fayyazzadeh <raminfayyaz@live.com>
2020-04-02 10:53:45 +0430

    new calender in 1399

--- contents/calendar.php
+++ contents/calendar.php
@@ -1,18 +1,19 @@
 <p dir="rtl" style="text-align: right;">&nbsp;</p>
-<p dir="rtl" style="text-align: right;">برنامه جلسات گروه کاربران لینوکس تهران در سال ۱۳۹۸ به شرح ذیل می‌باشد:<br>نیمه اول سال، ساعت ۱۴ الی ۱۸<br>
-<strike>۲۸ فروردین</strike><br>
-<strike>۱۸ اردیبهشت</strike><br>
-<strike>۲۲ خرداد</strike><br>
-<strike>۲۶ تیر</strike><br>
-<strike>۳۰ مرداد</strike><br>
-<strike>۲۷ شهریور</strike><br>
+<p dir="rtl" style="text-align: right;">برنامه جلسات گروه کاربران لینوکس تهران در سال ۱۳۹۹ به شرح ذیل می‌باشد:<br>نیمه اول سال، ساعت ۱۴ الی ۱۸<br>
+۲۸ فروردین<br>
+۱۸ اردیبهشت<br>
+۲۲ خرداد<br>
+۲۶ تیر<br>
+۳۰ مرداد<br>
+۲۷ شهریور<br>
 <br>نیمه دوم سال، ساعت ۱۳ الی ۱۸<br>
-<strike>۲۴ مهرماه</strike><br>
-<strike>۲۹ آبان</strike><br>
-<strike>۲۷ آذر</strike><br>
-<strike>۱۸ دی</strike>
-<br>۳۰ بهمن<br>
-<br> ۱۴ اسفند<br>
+۲۴ مهرماه<br>
+۲۹ آبان<br>
+۲۷ آذر<br>
+۱۸ دی<br>
+۳۰ بهمن<br>
+۱۴ اسفند<br>
+<br />
 </p>
 <p class="notice" dir="rtl" style="text-align: right;">استفاده از خدمات تقویم گوگل برای اطلاع‌رسانی جلسات توسط تهران‌لاگ به معنی تایید کارکرد این شرکت نبوده و گروه کاربران گنو/لینوکس تهران مسئولیتی در رابطه با سیاست حفظ اسرار این شرکت ندارد.</p>
 برای افزودن برنامه‌ی جلسات گروه کاربران لینوکس تهران به سرویس تقویم گوگل، <a href="https://calendar.google.com/calendar?cid=MHY3M3Z0czVoYnU5N3Nmajk0Mzdtcmc1cGNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ" target="_blank">صفحه‌ی تقویم جلسات تهران لاگ</a> را باز کنید و در گوشه‌ی سمت راست پایین صفحه روی علامت&nbsp;<img src="images/calendar_plus_en.gif" /> کلیک کنید.<div dir="rtl">&nbsp;</div>
